c0d564b6ca3de985c0535c289a1dd68eb1a98aed
saikumar-22-2003/ <jennesaikumar589@gmail.com>
2026-06-24 14:00:16 +0530

    Project Completed

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_pic from './profile_pic.png'
 import contact_image from './contact_image.png'
 import about_image from './about_image.png'
 import logo from './logo.svg'
+import medivue_logo from './medivue_logo.svg'
 import dropdown_icon from './dropdown_icon.svg'
 import menu_icon from './menu_icon.svg'
 import cross_icon from './cross_icon.png'
@@ -13,10 +14,8 @@ import verified_icon from './verified_icon.svg'
 import arrow_icon from './arrow_icon.svg'
 import info_icon from './info_icon.svg'
 import upload_icon from './upload_icon.png'
-import tick_icon from './tick_icon.svg'
 import stripe_logo from './stripe_logo.png'
 import razorpay_logo from './razorpay_logo.png'
-import medibook_logo from './medibook_logo.svg'
 import doc1 from './doc1.png'
 import doc2 from './doc2.png'
 import doc3 from './doc3.png'
@@ -45,6 +44,7 @@ export const assets = {
     header_img,
     group_profiles,
     logo,
+    medivue_logo,
     chats_icon,
     verified_icon,
     info_icon,
@@ -56,7 +56,6 @@ export const assets = {
     cross_icon,
     dropdown_icon,
     upload_icon,
-    tick_icon,
     stripe_logo,
     razorpay_logo
 }
@@ -299,4 +298,5 @@ export const doctors = [
             line2: 'Circle, Ring Road, London'
         }
     },
-]
+]  
+
