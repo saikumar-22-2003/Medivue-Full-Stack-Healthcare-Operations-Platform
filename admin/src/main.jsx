ade5c1c9bf99078fd8fa7a4b89c2b88b785539c6
saikumar-22-2003/ <jennesaikumar589@gmail.com>
2026-06-24 15:50:33 +0530

    Project Completed

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './assets/App.jsx'
+import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import AdminContextProvider from './context/AdminContext.jsx'
